9a5f0d7a5834f7a522efb2d1367e78c59e08710d
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-02-01 12:20:42 +0530

    new test files added

--- 1.findLenght/test_findlenght.py
+++ 1.findLenght/test_findlenght.py
@@ -1,3 +1,13 @@
+import findlenght
 def test_assertTrue():
     assert True
-    
+
+
+def test_listreturn6():
+    #arrange
+    array = [1,2,3,4,5,6]
+    excepted = 6
+    #act
+    actual = findlenght.len(array)
+    #assert
+    assert excepted == actual
