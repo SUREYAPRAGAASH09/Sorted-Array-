d5a643b5aba235a8f75498ca71fe27619586c9bc
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-03-08 15:23:54 +0530

    Code Revised Completely

---
+++ 2.isContains/test_isContains.py
@@ -0,0 +1,25 @@
+import isContain
+
+def test_AssertTrue():
+    assert True
+
+
+def test_array_retutn_5():
+    #arrange
+    array = [3,5,7,9,0,3]
+    Search_element = 3
+    excepted = True
+    #act
+    actual = isContain.find_element(array,Search_element)
+    #assert
+    assert excepted == actual 
+
+def test_array1_retutn_5():
+    #arrange
+    array = [3,5,7,9,0,3]
+    Search_element = 9
+    excepted = True
+    #act
+    actual = isContain.find_element(array,Search_element)
+    #assert
+    assert excepted == actual
