197e33fa8414fda6478da761c95ae0ce6ed43a95
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-03-08 15:15:43 +0530

    Code Revised Completely

--- 1.findLenght/findlenght.py
+++ 1.findLenght/findlenght.py
@@ -1,18 +1,21 @@
-#. Find the length of the given integer array. Don't use the len() function.
-#algorithm :
-        #1. I have added a string to the list and iterated the 
-        # list item untill the string is found using while Loop 
-        #I have used while loop because where we can't predict when the list get ends
+Problem Statement :
+===================        
+        Find the length of the given integer array. Don't use the len() function.
+Input :
+=======
+        Integer Array 
+Output :
+========
+        Integer - Lenght of the array
+      
+Code :
+=======        
         
-def len(array):
+def lenght(array):
     count = 0
-    i = 0
+    iterator = 0
     array.append("+-*/")
     while(array[i]!="+-*/"):
         count += 1
-        i+=1
+        iterator+=1
     return count 
-
-
-array = [1,2,3,4,5,6]
-print(len(array))
